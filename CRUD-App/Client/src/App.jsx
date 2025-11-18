0cd75e9d331eec49b0fe5a512554cfbb0eee2261
rajarya199 <aryalrajan078@gmail.com>
2025-11-19 00:04:32 +0545

    routes

--- CRUD-App/Client/src/App.jsx
+++ CRUD-App/Client/src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import AddUsers from './components/AddUsers'
+import AllUsers from './components/AllUsers';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
     <Navbar/>
-    </>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/all-users' element={<AllUsers/>}/>
+      <Route path='/add-user' element={<AddUsers/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
