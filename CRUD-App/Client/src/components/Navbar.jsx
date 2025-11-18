40c131481c6da6d4c866c3c65f9d25a7c5379d19
rajarya199 <aryalrajan078@gmail.com>
2025-11-18 22:08:00 +0545

    navbar-i

---
+++ CRUD-App/Client/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const Navbar = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          My Site
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
