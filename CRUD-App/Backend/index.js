f9fed6714eaadf06ca5f85d8fa1b545f7860881f
rajarya199 <aryalrajan078@gmail.com>
2025-11-20 21:31:14 +0545

    server-setup

--- CRUD-App/Backend/index.js
+++ CRUD-App/Backend/index.js
@@ -1,6 +1,17 @@
 import express from 'express'
-
+import dotenv from 'dotenv';
+import { connectDb } from './database/db.js';
+dotenv.config()
 const app=express()
-const PORT =8000
+connectDb()
+// Middleware to parse JSON
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Server is running...');
+});
+
+const PORT =process.env.PORT || 5000;
 app.listen(PORT,()=>console.log(`server is running on port ${PORT}`))
 
