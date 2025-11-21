39618dd701bd7468f889be979396a33768f21e5c
rajarya199 <aryalrajan078@gmail.com>
2025-11-21 22:11:15 +0545

    user-schema

---
+++ CRUD-App/Backend/models/user.model.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    address: {
+      type: String,
+      trim: true,
+    },
+    phone: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true, // adds createdAt and updatedAt timestamps
+  }
+);
+
+const User=mongoose.model('user',userSchema)
+export default User;
