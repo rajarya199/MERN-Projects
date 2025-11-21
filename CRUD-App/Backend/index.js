03fd34deadb450675bf7f3254ad66b1531683e1e
rajarya199 <aryalrajan078@gmail.com>
2025-11-21 21:04:49 +0545

    server

--- CRUD-App/Backend/index.js
+++ CRUD-App/Backend/index.js
@@ -1,12 +1,19 @@
 import express from 'express'
 import dotenv from 'dotenv';
+import userRoutes from './routes/route.js'
 import { connectDb } from './database/db.js';
+import cors from 'cors'
+import bodyParser from 'body-parser';
 dotenv.config()
 const app=express()
 connectDb()
+app.use(cors())
+app.use(bodyParser.json({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}))
 // Middleware to parse JSON
 app.use(express.json());
 
+app.use('/',userRoutes)
 
 app.get('/', (req, res) => {
   res.send('Server is running...');
