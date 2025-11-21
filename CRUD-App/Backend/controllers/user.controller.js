e7e91348a8278674357a95b6fe5378bba5315668
rajarya199 <aryalrajan078@gmail.com>
2025-11-21 22:11:49 +0545

    add user controller

---
+++ CRUD-App/Backend/controllers/user.controller.js
@@ -0,0 +1,26 @@
+import User from "../models/user.model.js"
+export const addUser=async(request,response)=>{
+  const { name, username, email, phone, address } = request.body;
+
+    try{
+           if (!name || !username || !email) {
+      return response.status(400).json({ message: 'Name, username, and email are required.' });
+    }
+
+     // Check if username or email already exists
+    const existingUser = await User.findOne({
+      $or: [{ username }, { email }],
+    });
+    if (existingUser) {
+      return response.status(400).json({ message: 'Username or email already exists.' });
+    }
+
+    const newUser = new User({ name, username, email, phone, address });
+
+    await newUser.save()
+        response.status(201).json(newUser)
+    }
+    catch(error){
+ response.status(500).json({message:error.message})
+    }
+}
