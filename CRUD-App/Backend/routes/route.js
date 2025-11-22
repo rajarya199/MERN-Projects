8b87123d3d5b42a3a3fc38c17fc855949873c3e5
rajarya199 <aryalrajan078@gmail.com>
2025-11-22 22:07:23 +0545

    route

--- CRUD-App/Backend/routes/route.js
+++ CRUD-App/Backend/routes/route.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import { addUser } from '../controllers/user.controller.js'
+import { addUser,getAllUsers } from '../controllers/user.controller.js'
 const router=express.Router()
 
 router.post('/add',addUser)
+router.get("/all",getAllUsers)
 
 export  default router
